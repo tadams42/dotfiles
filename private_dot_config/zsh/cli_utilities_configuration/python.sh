c0255708487cbc6d440f09eaa08e3ace4947a891
Tomislav Adamic <tomislav.adamic@gmail.com>
2026-06-18 11:19:34 +0200

    feat: cleaned up Python cleanup aliases

--- private_dot_config/zsh/cli_utilities_configuration/python.sh
+++ private_dot_config/zsh/cli_utilities_configuration/python.sh
@@ -11,11 +11,4 @@ export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
 
 alias py.spec='py.test -p no:sugar --spec'
 
-alias tar_py='tar --exclude="*/__pycache__" --exclude="*.pyc" --exclude="*/.venv" --exclude="*/.tox" --exclude="*/.vagrant"'
-alias tar_py_src='tar --verbose --create --exclude="*/_build" --exclude="*/build" --exclude="*.pyc" --exclude="*/__pycache__" --exclude="*/.venv" --exclude="*/.external_pip_packages" --exclude="*/log/*" --exclude="*/.cache" --exclude="*/.vagrant" --exclude="*/.benchmarks" --exclude="*/dist"'
-
-alias py.clean="find ./ \( -type d -and -name __pycache__ -or -name '.pytest_cache' \) -or \( -type f -name '*.pyc' \) | xargs rm -rf"
-alias py.distclean="find ./ \( -type d -wholename '*/src/*.egg-info' -or -name dist -or -wholename '*/docs/_build' \) | xargs rm -rf; py.clean"
-alias py_envclean="find ./ \( -type d -name .venv -or -name .tox \) | xargs rm -rf; py.distclean"
-
-# alias py.cloc='cloc --exclude-dir=_build,.venv,.venv2,.venv3,.tox,.vscode,log,build,dist .'
+alias py.envclean='fd -H -I -t d --prune "^(__pycache__|\.pytest_cache|\.venv|\.tox|dist|_build)$|\.egg-info$" -X rm -rf; fd -H -I -t f -e pyc -X rm'
