9ef8bdf19ead1930de7a1eb04892c6d60ce42793
Tomislav Adamic <tomislav.adamic@gmail.com>
2026-08-22 09:15:10 +0200

    fix: make run-after-build be silent

--- run_after_build-bat-cache.sh
+++ run_after_build-bat-cache.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 if command -v bat >/dev/null 2>&1; then
-    bat cache --build
+    # order matters: error messages still print to your terminal while normal output is
+    # silenced.
+    bat cache --build 2>&1 >/dev/null
 fi
