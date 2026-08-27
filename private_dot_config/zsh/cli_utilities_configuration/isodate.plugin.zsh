66bba7dba21ed7e60314a9238086ea308c732db2
Tomislav Adamic <tomislav.adamic@gmail.com>
2026-08-27 10:00:58 +0200

    feat: relocated few dotfiles to `XDG_CONFIG_HOME`

--- private_dot_config/zsh/cli_utilities_configuration/isodate.plugin.zsh
+++ private_dot_config/zsh/cli_utilities_configuration/isodate.plugin.zsh
@@ -1,7 +1,6 @@
 # C/P from oh-my-zsh
 
 # work with date ISO 8601 easy
-
 alias isodate="date +%Y-%m-%dT%H:%M:%S%z"
 alias isodate_utc="date -u +%Y-%m-%dT%H:%M:%SZ"
 alias isodate_basic="date -u +%Y%m%dT%H%M%SZ"
